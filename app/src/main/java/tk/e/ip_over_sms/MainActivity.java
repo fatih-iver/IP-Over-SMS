ebf188defefbcfe92806198cc9dfb93bc4e97e18
fatih-iver <fatih.iver@boun.edu.tr>
2019-12-09 22:01:30 +0300

    initialize project

---
+++ app/src/main/java/tk/e/ip_over_sms/MainActivity.java
@@ -0,0 +1,14 @@
+package tk.e.ip_over_sms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
